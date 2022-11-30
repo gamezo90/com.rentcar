0c77984f160b7d77009d87851e281474d2aed74e
Nick <otulan@example.com>
2022-11-30 19:54:44 +0300

    Discount

---
+++ common/src/main/java/com/rentcar/service/impl/DiscountServiceImpl.java
@@ -0,0 +1,33 @@
+package com.rentcar.service.impl;
+
+import com.rentcar.domain.Discount;
+import com.rentcar.service.DiscountSystem;
+
+import java.util.List;
+
+public class DiscountServiceImpl implements DiscountSystem {
+    @Override
+    public Discount findByUserId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Discount findByUserLogin(String login) {
+        return null;
+    }
+
+    @Override
+    public List<Discount> findAll() {
+        return null;
+    }
+
+    @Override
+    public Discount create(Discount discount) {
+        return null;
+    }
+
+    @Override
+    public Discount update(Discount discountToUpdate) {
+        return null;
+    }
+}
