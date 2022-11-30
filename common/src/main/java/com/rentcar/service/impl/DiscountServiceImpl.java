4c4b2e22c53f35c393e5be653eb15aac1bf238a4
Nick <gamezo90@mail.ru>
2022-11-30 20:09:27 +0300

    Add DiscountSystemImpl

--- common/src/main/java/com/rentcar/service/impl/DiscountServiceImpl.java
+++ common/src/main/java/com/rentcar/service/impl/DiscountServiceImpl.java
@@ -1,33 +1,50 @@
 package com.rentcar.service.impl;
 
 import com.rentcar.domain.Discount;
+import com.rentcar.domain.Order;
+import com.rentcar.exception.NoSuchEntityException;
+import com.rentcar.repository.DiscountRepository;
 import com.rentcar.service.DiscountSystem;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
 public class DiscountServiceImpl implements DiscountSystem {
+
+    private final DiscountRepository discountRepository;
     @Override
     public Discount findByUserId(Long id) {
-        return null;
+        return discountRepository.findByUserId(id);
     }
 
     @Override
     public Discount findByUserLogin(String login) {
-        return null;
+        return discountRepository.findByUserCredentialsLogin(login);
     }
 
     @Override
     public List<Discount> findAll() {
-        return null;
+        return discountRepository.findAll();
     }
 
     @Override
     public Discount create(Discount discount) {
-        return null;
+        discount.setCreationDate(new Timestamp(new Date().getTime()));
+        discount.setModificationDate(new Timestamp(new Date().getTime()));
+        discountRepository.save(discount);
+
+        return discountRepository.findById(discount.getId()).orElseThrow(IllegalArgumentException::new);
+
     }
 
     @Override
     public Discount update(Discount discountToUpdate) {
-        return null;
+        discountToUpdate.setModificationDate(new Timestamp(new Date().getTime()));
+        discountRepository.save(discountToUpdate);
+        return discountRepository.findById(discountToUpdate.getId()).orElseThrow(IllegalArgumentException::new);
     }
 }
