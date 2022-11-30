46b9a664e2d21ecbba895dc07ef4f5f6f8e6ce6f
Nick <gamezo90@mail.ru>
2022-11-30 20:24:51 +0300

    Fix DiscountSystem

--- api/src/main/java/com/rentcar/controller/DiscountController.java
+++ api/src/main/java/com/rentcar/controller/DiscountController.java
@@ -1,14 +1,18 @@
 package com.rentcar.controller;
 
 import com.rentcar.controller.mappers.DiscountMapper;
+import com.rentcar.controller.requests.DiscountRequests.DiscountCreateRequest;
+import com.rentcar.controller.requests.DiscountRequests.DiscountUpdateRequest;
+import com.rentcar.controller.response.DiscountResponse;
+import com.rentcar.domain.Discount;
 import com.rentcar.service.DiscountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import java.util.Collections;
 
 @RestController
