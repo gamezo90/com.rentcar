b462a0ade8085d20362da7f7842463c71a1d8408
Nick <otulan@example.com>
2022-12-20 19:56:56 +0300

    add exceptions to DiscountServiceImpl

--- common/src/main/java/com/rentcar/service/impl/DiscountServiceImpl.java
+++ common/src/main/java/com/rentcar/service/impl/DiscountServiceImpl.java
@@ -1,20 +1,30 @@
 package com.rentcar.service.impl;
 
 import com.rentcar.domain.Discount;
+import com.rentcar.domain.User;
 import com.rentcar.repository.DiscountRepository;
+import com.rentcar.repository.UserRepository;
 import com.rentcar.service.DiscountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class DiscountServiceImpl implements DiscountService {
 
     private final DiscountRepository discountRepository;
+
+    private final UserServiceImpl userService;
+
+    private final UserRepository userRepository;
+
     @Override
     public Discount findByUserId(Long id) {
         if (discountRepository.findByUserId(id) == null) {
@@ -57,5 +67,17 @@ public class DiscountServiceImpl implements DiscountService {
         discountToUpdate.setModificationDate(new Timestamp(new Date().getTime()));
         discountRepository.save(discountToUpdate);
         return discountRepository.findById(discountToUpdate.getId()).orElseThrow(IllegalArgumentException::new);
+    }
+
+    public void checkUserIdForNotExist(Long userId) {
+        userService.findById(userId);
+      }
+
+    public void checkUserDiscountAlreadyExists(Long userId) {
+        if (userRepository.findById(userId).isPresent()){
+            throw new EntityExistsException(
+                    String.format("User discount with this id %s already exists", userId));
+        }
+
     }
 }
