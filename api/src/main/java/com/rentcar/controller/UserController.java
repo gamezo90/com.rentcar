2ffffe5e7e4bef0676411b6ab74db949c0dec60b
Nick <gamezo90@mail.ru>
2023-01-04 21:44:49 +0300

    Swagger 3 in progress

--- api/src/main/java/com/rentcar/controller/UserController.java
+++ api/src/main/java/com/rentcar/controller/UserController.java
@@ -6,8 +6,9 @@ import com.rentcar.controller.response.UserResponse;
 import com.rentcar.domain.User;
 import com.rentcar.service.RoleService;
 import com.rentcar.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Collections;
 import java.util.Map;
-
+@Tag(name = "User controller")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -27,26 +28,26 @@ public class UserController {
 
     private final RoleService roleService;
 
-
-    @GetMapping("/findAllUser")
-    public ResponseEntity<Object> findAllUser() {
+    @Operation(summary = "Find all users")
+    @GetMapping("/findAllUsers")
+    public ResponseEntity<Object> findAllUsers() {
 
         return new ResponseEntity<>(Collections.singletonMap("result", userService.findAll()), HttpStatus.OK);
     }
-
+    @Operation(summary = "Find user by id")
     @GetMapping("/findUserById/{id}")
-    public ResponseEntity<Map<String, Object>> findUserById(@PathVariable String id) {
-        Long userId = Long.parseLong(id);
+    public ResponseEntity<Object> findUserById(@PathVariable Long userId) {
+      //  Long userId = Long.parseLong(id);
         UserResponse user = userMapper.toResponse(userService.findById(userId));
         return new ResponseEntity<>(Collections.singletonMap("user", user), HttpStatus.OK);
     }
-
+    @Operation(summary = "Find user by login")
     @GetMapping("/findUserByLogin")
     public ResponseEntity<Object> findUserByLogin(@RequestParam("user_login") String login) {
 
         return new ResponseEntity<>(Collections.singletonMap("result", userService.findByLogin(login)), HttpStatus.OK);
     }
-
+    @Operation(summary = "Soft delete user by login")
     @PatchMapping("/softDeleteUserByLogin/{login}")
     public ResponseEntity<Object> softDeleteUserByLogin(@PathVariable("login") String login) {
 
@@ -54,7 +55,7 @@ public class UserController {
 
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-
+    @Operation(summary = "Ban delete user by login")
     @PatchMapping("/banUserByLogin/{login}")
     public ResponseEntity<Object> banUserByLogin(@PathVariable("login") String login) {
 
@@ -62,7 +63,7 @@ public class UserController {
 
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-
+    @Operation(summary = "Update user by login")
     @PutMapping(value = "/updateUser/{login}")
     public ResponseEntity<Object> updateUser(@RequestParam("user_login") String login, @Valid @RequestBody UserUpdateRequest request) {
         User updatedUser = userMapper.convertUpdateRequest(request, userService.findByLogin(login));
@@ -70,18 +71,17 @@ public class UserController {
         UserResponse userResponse = userMapper.toResponse(userService.update(updatedUser));
         return new ResponseEntity<>(Collections.singletonMap("user", userResponse), HttpStatus.OK);
     }
-
+    @Operation(summary = "Add role to user by login")
     @PatchMapping("/addRoleToUserByLogin/{login}")
     public ResponseEntity<Object> addRoleToUserByLogin(@PathVariable("login")String login, String role) {
         User user = userService.addRoleToUser(userService.findByLogin(login), roleService.findRoleByName(role));
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-
+    @Operation(summary = "Remove user role")
     @PatchMapping("/removeUserRole/{login}")
     public ResponseEntity<Object> removeUserRole(@PathVariable("login")String login, String role) {
         User user = userService.removeUserRole(userService.findByLogin(login), roleService.findRoleByName(role));
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-
 
 }
