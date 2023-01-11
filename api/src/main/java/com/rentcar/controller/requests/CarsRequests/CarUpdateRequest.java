638c5db202703a54e0f645e8b90a6c1011f143e2
Nick <otulan@example.com>
2023-01-11 20:40:21 +0300

    swagger 3 refactoring

--- api/src/main/java/com/rentcar/controller/requests/CarsRequests/CarUpdateRequest.java
+++ api/src/main/java/com/rentcar/controller/requests/CarsRequests/CarUpdateRequest.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
@@ -11,32 +12,33 @@ import javax.validation.constraints.Size;
 @Schema(description = "Car update request")
 public class CarUpdateRequest {
 
-    @Schema(defaultValue = "3.6", type = "Float" , description = "Car engine volume")
-    @Positive
-    private Float engineVolume;
+    @Schema(defaultValue = "3.6", type = "String" , description = "Car engine volume")
+    @Pattern(regexp = "^\\d+(?:[\\.]\\d+)?$", message = "Engine volume must be positive floating point numbers")
+    private String engineVolume;
 
-    @Schema(defaultValue = "True", type = "Boolean" , description = "Сonditioner")
-    @NotNull
-    private Boolean conditioner;
+    @Schema(defaultValue = "Yes", type = "String" , description = "Conditioner")
+    private String conditioner;
 
     @Schema(defaultValue = "Red", type = "String" , description = "Car color")
-    @Size(min = 1, max = 20)
+    @Size(min = 2, max = 20)
+    @Pattern(regexp = "^([A-Za-z]([ -])?)*$", message = "Color must be string with space or dash")
     private String color;
 
     @Schema(defaultValue = "Some Photo", type = "String" , description = "Car photo")
-    @Size(min = 1, max = 255)
+    @Size(min = 2, max = 255)
     private String photo;
 
     @Schema(defaultValue = "Gomel", type = "String" , description = "Car region")
+    @Pattern(regexp = "^[a-zA-Z]+[ ]?[a-zA-Z]+[ ]?[a-zA-Z]*$", message = "Region must be maximum three words")
     @Size(min = 2, max = 30)
     private String region;
 
-    @Schema(defaultValue = "500", type = "Double" , description = "Car price")
-    @Positive
-    private Double price;
+    @Schema(defaultValue = "500", type = "String" , description = "Car price")
+    @Pattern(regexp = "^\\d+(?:[\\.]\\d+)?$", message = "Price must be positive floating point numbers")
+    private String price;
 
     @Schema(defaultValue = "9075IA", type = "String" , description = "Car registration number")
-    @Size(min = 1, max = 10)
+    @Pattern(regexp = "^\\d{4}[ ][A-Z]{2}[-][1-7]|[A-Z]{2}[ ]\\d{4}[-][1-7]", message = "Registration number pattern 1234 AA-1 or AA 1234-1")
     private String registrationNumber;
 
 }
