37e21804af3198bdaa2fba702691065e6d1ca854
Nick <gamezo90@mail.ru>
2023-01-26 21:13:51 +0300

    Spring security continue (users)

--- api/src/main/java/com/rentcar/controller/UserController.java
+++ api/src/main/java/com/rentcar/controller/UserController.java
@@ -7,11 +7,15 @@ import com.rentcar.domain.User;
 import com.rentcar.service.RoleService;
 import com.rentcar.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,25 +33,45 @@ public class UserController {
 
     private final RoleService roleService;
 
-    @Operation(summary = "Find all users")
+
+    @Operation(summary = "Find all users", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasAnyRole('MODERATOR', 'ADMIN')")
     @GetMapping("/findAllUsers")
     public ResponseEntity<Object> findAllUsers() {
 
         return new ResponseEntity<>(Collections.singletonMap("result", userService.findAll()), HttpStatus.OK);
     }
-    @Operation(summary = "Find user by id")
+
+    @Operation(summary = "Find user by id", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasAnyRole('MODERATOR', 'ADMIN')")
     @GetMapping("/findUserById/{id}")
         public ResponseEntity<Object> findUserById(@RequestParam("userId") Long userId) {
         UserResponse userResponse = userMapper.toResponse(userService.findById(userId));
         return new ResponseEntity<>(Collections.singletonMap("user", userResponse), HttpStatus.OK);
     }
-    @Operation(summary = "Find user by login")
+
+    @Operation(summary = "Find user by login", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasAnyRole('MODERATOR', 'ADMIN')")
     @GetMapping("/findUserByLogin")
     public ResponseEntity<Object> findUserByLogin(@RequestParam("user_login") String login) {
 
         return new ResponseEntity<>(Collections.singletonMap("result", userService.findByLogin(login)), HttpStatus.OK);
     }
-    @Operation(summary = "Soft delete user by login")
+
+    @Operation(summary = "Soft delete user by login", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasRole('ADMIN')")
     @PatchMapping("/softDeleteUserByLogin/{login}")
     public ResponseEntity<Object> softDeleteUserByLogin(@PathVariable("login") String login) {
 
@@ -55,7 +79,12 @@ public class UserController {
 
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-    @Operation(summary = "Ban delete user by login")
+
+    @Operation(summary = "Ban delete user by login", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasRole('ADMIN')")
     @PatchMapping("/banUserByLogin/{login}")
     public ResponseEntity<Object> banUserByLogin(@PathVariable("login") String login) {
 
@@ -63,7 +92,12 @@ public class UserController {
 
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-    @Operation(summary = "Update user by login")
+
+    @Operation(summary = "Update user by login", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasRole('USER')")
     @PutMapping(value = "/updateUser/{login}")
     public ResponseEntity<Object> updateUser(@RequestParam("user_login") String login, @Valid @RequestBody UserUpdateRequest request) {
         User updatedUser = userMapper.convertUpdateRequest(request, userService.findByLogin(login));
@@ -71,18 +105,26 @@ public class UserController {
         UserResponse userResponse = userMapper.toResponse(userService.update(updatedUser));
         return new ResponseEntity<>(Collections.singletonMap("user", userResponse), HttpStatus.OK);
     }
-    @Operation(summary = "Add role to user by login")
+
+    @Operation(summary = "Add role to user by login", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasRole('ADMIN')")
     @PatchMapping("/addRoleToUserByLogin/{login}")
     public ResponseEntity<Object> addRoleToUserByLogin(@PathVariable("login")String login, String role) {
         User user = userService.addRoleToUser(userService.findByLogin(login), roleService.findRoleByName(role));
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-    @Operation(summary = "Remove user role")
+
+    @Operation(summary = "Remove user role", parameters = {
+            @Parameter(in = ParameterIn.HEADER, name = "X-Auth-Token", description = "Token", required = true,
+                    schema = @Schema(defaultValue = "token", type = "string"))
+    })
+    @PreAuthorize(value = "hasRole('ADMIN')")
     @PatchMapping("/removeUserRole/{login}")
     public ResponseEntity<Object> removeUserRole(@PathVariable("login")String login, String role) {
         User user = userService.removeUserRole(userService.findByLogin(login), roleService.findRoleByName(role));
         return new ResponseEntity<>(Collections.singletonMap(user, userMapper.toResponse(user)), HttpStatus.OK);
     }
-
-
 }
